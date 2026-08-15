5fc4633baeb1333dd995d9470ba4db4ce57af2b9
ayushagra24 <ayushmp0410.in@gmail.com>
2026-08-15 23:22:01 +0530

    Random array generation of decimal numbers and integers

---
+++ random_array.m
@@ -0,0 +1,19 @@
+%Generation of 3x4 array with decimal numbers from below code 
+% clc;
+% clear;
+% close all;
+% 
+% A = rand(3,4);
+% 
+% disp('Random Array:');
+% disp(A);
+
+%Generation of 3x4 array with integers from below code 
+clc;
+clear;
+close all;
+
+A = randi(10,3,4);
+
+disp('Random Integer Array:');
+disp(A);
