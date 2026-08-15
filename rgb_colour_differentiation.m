42450862f8f9326871bead762c77231f09a8e211
ayushagra24 <ayushmp0410.in@gmail.com>
2026-08-15 22:53:05 +0530

    rgb_colour_differentiation.m

--- rgb_colour_differentiation.m
+++ rgb_colour_differentiation.m
@@ -2,34 +2,34 @@ clc;
 clear;
 close all;
 
-% Read image
 img = imread('colors.jpg');
 
-% Separate RGB channels
 R = img(:,:,1);
 G = img(:,:,2);
 B = img(:,:,3);
 
-% Find red, green and blue pixels
-red = (R > G) & (R > B) & (R > 100);
-green = (G > R) & (G > B) & (G > 100);
-blue = (B > R) & (B > G) & (B > 100);
+red_img = zeros(size(img), 'uint8');
+green_img = zeros(size(img), 'uint8');
+blue_img = zeros(size(img), 'uint8');
 
-% Display results
+red_img(:,:,1) = R;
+green_img(:,:,2) = G;
+blue_img(:,:,3) = B;
+
 figure;
 
-subplot(2,2,1);
+subplot(4,1,1);
 imshow(img);
 title('Original Image');
 
-subplot(2,2,2);
-imshow(red);
-title('Red Pixels');
+subplot(4,1,2);
+imshow(red_img);
+title('Red Channel');
 
-subplot(2,2,3);
-imshow(green);
-title('Green Pixels');
+subplot(4,1,3);
+imshow(green_img);
+title('Green Channel');
 
-subplot(2,2,4);
-imshow(blue);
-title('Blue Pixels');
+subplot(4,1,4);
+imshow(blue_img);
+title('Blue Channel');
